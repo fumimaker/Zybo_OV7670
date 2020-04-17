a87de47ae09a138607fce9114be7a08dc41aa0ae
fumi <fumi01200924@gmail.com>
2020-04-18 03:35:05 +0900

    PsからPLのGPIOをAXIを通してKチカした

---
+++ Ps_AXIPLGPIO_Lchika/.sdk/launch_scripts/single_application_debug/systemdebugger_axigpio_system_standalone.tcl
@@ -0,0 +1,18 @@
+connect -url tcp:127.0.0.1:3121
+targets -set -nocase -filter {name =~"APU*"}
+rst -system
+after 3000
+targets -set -filter {jtag_cable_name =~ "Digilent Zybo Z7 210351A6C9E3A" && level==0} -index 1
+fpga -file C:/Users/ivr/Documents/GitHub/Zybo_OV7670/Ps_AXIPLGPIO_Lchika/AXIGPIO/_ide/bitstream/design_1_wrapper.bit
+targets -set -nocase -filter {name =~"APU*"}
+loadhw -hw C:/Users/ivr/Documents/GitHub/Zybo_OV7670/Ps_AXIPLGPIO_Lchika/design_1_wrapper/export/design_1_wrapper/hw/design_1_wrapper.xsa -mem-ranges [list {0x40000000 0xbfffffff}]
+configparams force-mem-access 1
+targets -set -nocase -filter {name =~"APU*"}
+source C:/Users/ivr/Documents/GitHub/Zybo_OV7670/Ps_AXIPLGPIO_Lchika/AXIGPIO/_ide/psinit/ps7_init.tcl
+ps7_init
+ps7_post_config
+targets -set -nocase -filter {name =~ "*A9*#0"}
+dow C:/Users/ivr/Documents/GitHub/Zybo_OV7670/Ps_AXIPLGPIO_Lchika/AXIGPIO/Debug/AXIGPIO.elf
+configparams force-mem-access 0
+targets -set -nocase -filter {name =~ "*A9*#0"}
+con
