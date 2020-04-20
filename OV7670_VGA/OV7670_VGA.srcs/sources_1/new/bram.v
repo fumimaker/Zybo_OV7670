1cbf1eeb68ac392881df757665c13acccee93118
fumi <fumi01200924@gmail.com>
2020-04-21 04:50:05 +0900

    DiagramでBram作ってみたけど、結局Verilogで生成したほうが簡単な気がしてきた。

--- OV7670_VGA/OV7670_VGA.srcs/sources_1/new/bram.v
+++ OV7670_VGA/OV7670_VGA.srcs/sources_1/new/bram.v
@@ -21,6 +21,29 @@
 
 
 module bram(
-
+    input CLK,
+    input RST,
+    input wire [18:0] addr,
+    input [7:0] din,
+    output reg [7:0] dout,
+    input we,
+    input re
     );
+     (* ram_style = "BLOCK" *) reg [15:0] bram[0:18]; 
+     
+     always @(posedge CLK) begin
+        if (RST) begin
+            integer j;
+            for (j = 0; j < 524288;  j=j+1)
+                bram[j] = {16{1'b0}};
+            end
+        else begin
+            if (we) begin
+                bram[addr] <= din;
+                end
+            if (re) begin
+                dout <= bram[addr];
+                end
+            end
+    end
 endmodule
